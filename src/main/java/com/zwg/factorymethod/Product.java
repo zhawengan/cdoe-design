b146346c6b2c9df2e58dbf25c5071077833d132f
zwg <zwg@blueocean-health.com>
2017-12-08 17:12:17 +0800

    add factorymethod

---
+++ src/main/java/com/zwg/factorymethod/Product.java
@@ -0,0 +1,8 @@
+package com.zwg.factorymethod;
+
+/**
+ * Created by zwg.BlueOcean on 2017/12/8.
+ */
+//定义一个产品接口
+public interface Product {
+}
---
+++ src/main/java/com/zwg/factorymethod/WasherFactory.java
@@ -0,0 +1,10 @@
+package com.zwg.factorymethod;
+
+/**
+ * Created by zwg.BlueOcean on 2017/12/8.
+ */
+public class WasherFactory implements Factory{
+    public Product create() {
+        return new Washer();
+    }
+}
